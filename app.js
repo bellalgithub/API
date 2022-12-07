f133c9cf63a9c582d17a59c608941816239f8d5c
Bellal <bellalsq@gmail.com>
2022-12-07 12:26:45 +0600

    100 post updated

--- app.js
+++ app.js
@@ -26,3 +26,29 @@ function userData(data) {
         ul.appendChild(li);
     }
 }
+
+// API practice for 100 post 
+
+function loadPost() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(data => usersPost(data))
+
+}
+loadPost();
+const up = document.getElementById('users-post');
+function usersPost(data) {
+    for (const post of data) {
+        const li = document.createElement("li");
+        li.classList.add('userPost')
+        li.innerHTML = `
+        <h2>ID: ${post.id}</h2>
+        <h3>Title: ${post.title}</h3>
+        <h4>Post: ${post.body}</h4> 
+        ` ;
+        up.appendChild(li);
+    }
+
+}
+
+
