5da20d535e906fb0fe904a0a1685e848eed2a8e5
Bellal <bellalsq@gmail.com>
2022-12-14 02:00:39 +0600

    1st and 2nd class API updated

--- class-2.js
+++ class-2.js
@@ -43,3 +43,58 @@ const user = data => {
 
 
 }
+
+
+// World Countries Tour for Practice API
+
+const countryLoad = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(response => response.json())
+        .then(country => countries(country))
+}
+countryLoad();
+
+const countries = data => {
+
+    // console.log(data);
+    const countriesDiv = document.getElementById('country-list')
+    data.forEach(countryList => {
+        // console.log(countryList);
+        const countryDiv = document.createElement('div');
+        countryDiv.classList.add('country-class');
+        countryDiv.innerHTML = `
+        <h2>Country Name: ${countryList.name.common}</h2>
+        <h3>Capital : ${countryList.capital}</h3>
+        <h4>Population : ${countryList.population}</h4>
+        <button onclick ="countryByName('${countryList.name.common}')">Show Details</button>
+        `;
+
+        countriesDiv.appendChild(countryDiv);
+
+
+    });
+
+
+}
+
+const countryByName = (name) => {
+    const url = `https://restcountries.com/v3.1/name/${name} `
+    fetch(url)
+        .then(response => response.json())
+        .then(data => country(data[0]))
+}
+
+const country = (countryName) => {
+
+    // console.log(countryName);
+    const countryDetails = document.getElementById('country-details');
+    countryDetails.innerHTML = `
+    
+    <h2>Country Name: ${countryName.name.common}</h2>
+        <h3>Capital : ${countryName.capital}</h3>
+        <h4>Population : ${countryName.population}</h4>
+        <img src="${countryName.flags.png}" alt="">
+        
+    `
+
+}
